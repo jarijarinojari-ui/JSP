a2e8f8e4728876776e616442fcb4832f2d936113
kjg010829@naver.com <jarijarinojari@gmail.com>
2026-01-05 19:56:31 +0900

    commit

--- JSPBook2/src/main/java/ch04/com/dao/Person.java
+++ JSPBook2/src/main/java/ch04/com/dao/Person.java
@@ -7,5 +7,21 @@ public class Person {
 	public Person() {
 		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
